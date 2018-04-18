dd9cf2ddc9b230a658571cb9edd5cfaa353b5c5a
Timmy Truong <tagon1228@gmail.com>
2018-04-18 17:13:27 +0200

    Add register form to frontend

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component'
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,10 +16,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component'
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
